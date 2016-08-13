920abb4dd1ca2545527c006eadfdac4fa4cdca02
YL <Y@L>
2016-08-13 21:46:46 +1200

    Fixed Docker Kernel build issue
    Caused by missing "kmod" in Dockerfile

--- arm64/build_kernel_docker.sh
+++ arm64/build_kernel_docker.sh
@@ -4,7 +4,7 @@ DOCKER_IMAGE=$1
 [ -z "${DOCKER_IMAGE}" ] && exit 1
 
 KERNEL_PATH="/srv/kernel"
-docker run -i --rm -u root --privileged \
+docker run -i --rm -u root \
        -v "${KERNEL_PATH}":/srv/kernel \
        "${DOCKER_IMAGE}" /bin/bash -s <<EOF
 
@@ -12,16 +12,19 @@ set -x
 
 cd /srv/kernel
 [ -d linux-pine64 ] && rm -fr linux-pine64
+git clone --progress --depth 1 --single-branch -b pine64-hacks-1.2 https://github.com/longsleep/linux-pine64
+
 [ -d kernel-output ] && rm -fr kernel-output
 mkdir -p kernel-output/boot/pine64
 
-git clone --progress --depth 1 --single-branch -b pine64-hacks-1.2 https://github.com/longsleep/linux-pine64
-curl -sSL https://github.com/longsleep/build-pine64-image/raw/master/blobs/pine64.dts > linux-pine64/arch/arm64/boot/dts/sun50i-a64-pine64-plus.dts
-curl -sSL https://github.com/yang-l/pine64-kernel/raw/config/config-3.10 -o linux-pine64/.config
-
 cd linux-pine64
-make LOCALVERSION= clean
-make LOCALVERSION= oldconfig
+make clean mrproper
+
+# curl necessary files
+curl -sSL https://github.com/longsleep/build-pine64-image/raw/master/blobs/pine64.dts -o arch/arm64/boot/dts/sun50i-a64-pine64-plus.dts
+curl -sSL https://github.com/yang-l/pine64-kernel/raw/config/config-3.10 -o .config
+
+make oldconfig
 make -j4 LOCALVERSION= Image sun50i-a64-pine64-plus.dtb modules
 
 cp arch/arm64/boot/dts/sun50i-a64-pine64-plus.dtb arch/arm64/boot/Image ./.config ../kernel-output/boot/pine64
