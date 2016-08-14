14e2f38919bd7d714a629cb7f9957aa8e44ab765
YL <Y@L>
2016-08-14 18:27:54 +1200

    Fixed kernel cross-compiling issues

--- x86_64/build_kernel_docker.sh
+++ x86_64/build_kernel_docker.sh
@@ -5,7 +5,6 @@ DOCKER_IMAGE=$1
 
 KERNEL_PATH="/srv/kernel"
 docker run -i --rm -u root \
-       --privileged --cap-add=ALL \
        -v "${KERNEL_PATH}":/srv/kernel \
        "${DOCKER_IMAGE}" /bin/bash -s <<EOF
 
@@ -13,16 +12,19 @@ set -x
 
 cd /srv/kernel
 [ -d linux-pine64 ] && rm -fr linux-pine64
+git clone --progress --depth 1 --single-branch -b pine64-hacks-1.2 https://github.com/longsleep/linux-pine64
+
 [ -d kernel-output ] && rm -fr kernel-output
 mkdir -p kernel-output/boot/pine64
 
-git clone --progress --depth 1 --single-branch -b pine64-hacks-1.2 https://github.com/longsleep/linux-pine64
-curl -sSL https://github.com/longsleep/build-pine64-image/raw/master/blobs/pine64.dts > linux-pine64/arch/arm64/boot/dts/sun50i-a64-pine64-plus.dts
-curl -sSL https://github.com/yang-l/pine64-kernel/raw/config/config-3.10 -o linux-pine64/.config
-
 cd linux-pine64
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LOCALVERSION= clean
-make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LOCALVERSION= oldconfig
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- clean mrproper
+
+# curl necessary files
+curl -sSL https://github.com/longsleep/build-pine64-image/raw/master/blobs/pine64.dts -o arch/arm64/boot/dts/sun50i-a64-pine64-plus.dts
+curl -sSL https://github.com/yang-l/pine64-kernel/raw/config/config-3.10 -o .config
+
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- oldconfig
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4 LOCALVERSION= Image sun50i-a64-pine64-plus.dtb modules
 
 cp arch/arm64/boot/dts/sun50i-a64-pine64-plus.dtb arch/arm64/boot/Image ./.config ../kernel-output/boot/pine64
